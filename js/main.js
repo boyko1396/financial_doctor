1372e389e87ff330f53d8f95c503c145a129c9b6
Vasya <boyko1396@gmail.com>
2023-07-27 08:53:29 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
+// fullpage scroll init
+document.addEventListener('DOMContentLoaded', function() {
+    const layout = document.querySelector('body');
+    const fullpageInstance = new fullpage('#fullpage', {
+        css3: true,
+        scrollingSpeed: 800,
+        navigation: false,
+        slidesNavigation: false,
+        responsiveHeight: 330,
+        controlArrows: false,
+        fadingEffect: true,
+        anchors: ['intro', 'intro-hero', 'services', 'meta', 'appeal', 'footer']
+    });
+});
+
+//btn-toggle nav mobile
 const btnToggle = document.querySelectorAll('.js-btn-toggle');
 
 btnToggle.forEach((button) => {
-  button.addEventListener('click', () => {
-    const listCard = button.closest('.card');
+    button.addEventListener('click', () => {
+        const listCard = button.closest('.card');
 
-    if (listCard) {
-      listCard.classList.toggle('is-show');
-    }
-  });
+        if (listCard) {
+            listCard.classList.toggle('is-show');
+        }
+    });
+});
+
+// svg inline
+document.addEventListener('DOMContentLoaded', function() {
+    const svgContainers = document.querySelectorAll('.js-svg-inline');
+
+    svgContainers.forEach(container => {
+        const src = container.getAttribute('data-src');
+        const xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    const svgText = xhr.responseText;
+                    const wrapper = document.createElement('span');
+                    wrapper.innerHTML = svgText.trim();
+
+                    container.appendChild(wrapper.firstChild);
+                } else {
+                    console.error('Failed to load SVG:', src);
+                }
+            }
+        };
+
+        xhr.open('GET', src, true);
+        xhr.send();
+    });
 });
